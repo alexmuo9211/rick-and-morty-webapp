e7d3c994ba0b2342243e07b7f615c8902f631eb2
alexolivares9211 <alejandrolivares9211@gmail.com>
2025-05-02 12:45:58 -0600

    config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
+import { AppCacheProvider } from '@mui/material-nextjs/v15-pagesRouter';
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+     weight: ['300', '400', '500', '700'],
+      subsets: ['latin'],
+      display: 'swap',
+      variable: '--font-roboto',
+    });
+
+const theme = createTheme({
+      typography: {
+        fontFamily: 'var(--font-roboto)',
+      },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <AppCacheProvider {...pageProps}>
+          <ThemeProvider theme={theme}>
+                     <main className={roboto.variable}>
+      <Component {...pageProps} />
+                     </main>
+                   </ThemeProvider>
+      </AppCacheProvider>
+  );
 }
